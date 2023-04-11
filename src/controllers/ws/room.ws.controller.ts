639df9340df7c3880e5892303aa70d3f7829c92e
Nastya <shulhaevich@gmail.com>
2023-04-11 15:59:33 +0300

    fixed bug, can't change the name of the room name if it exists

--- src/controllers/ws/room.ws.controller.ts
+++ src/controllers/ws/room.ws.controller.ts
@@ -5,44 +5,45 @@ import { messageService } from "../../services/message.services";
 import { UserModel } from "../../models/user.model";
 
 export class RoomWsController {
-	constructor() { }
+   constructor() { }
 
-	public async joinRooms(client: Socket, userId: string, { rooms }: { rooms: string[] }) {
-		// save to model
-		const user = await UserModel.findById(userId);
-		await Promise.all(rooms.map(async (room) => {
-			const roomId = new Types.ObjectId(room);
-			await roomService.joinRoom(new Types.ObjectId(userId), roomId);
-			await client.join(room);
-		}));
-		//user-joined -> sends to FE. FE should have smth like socket.on("user-joined",
-		client.to(rooms).emit('user-joined', { message: `${user!.username} joined room` });
-	};
+   public async joinRooms(client: Socket, userId: string, { rooms }: { rooms: string[] }) {
 
-	public async leaveRooms(client: Socket, userId: string, { rooms }: { rooms: string[] }) {
-		const user = await UserModel.findById(userId);
-		console.log(userId);
+      // save to model
+      const user = await UserModel.findById(userId);
+      await Promise.all(rooms.map(async (room) => {
+         const roomId = new Types.ObjectId(room);
+         await roomService.joinRoom(new Types.ObjectId(userId), roomId);
+         await client.join(room);
+      }));
 
-		await Promise.all(rooms.map(async (room) => {
-			const roomId = new Types.ObjectId(room);
-			await roomService.leaveRoom(new Types.ObjectId(userId), roomId);
-			await client.leave(room);
-		}));
-		await Promise.all(rooms.map((room) => client.leave(room)))
-		client.to(rooms).emit('user-left', { message: `${user!.username} leave room` })
-	};
+      //user-joined -> sends to FE. FE should have smth like socket.on("user-joined",
+      client.to(rooms).emit('user-joined', { message: `${user!.username} joined room` });
+   };
 
-	public async sendMessageToRoom(client: Socket, userId: string, { room, message, filePath }: { room: string, message: string, filePath?: string }) {
-		const user = await UserModel.findById(userId);
-		await messageService.saveMessage(new Types.ObjectId(userId), new Types.ObjectId(room), message, filePath);
+   public async leaveRooms(client: Socket, userId: string, { rooms }: { rooms: string[] }) {
+      const user = await UserModel.findById(userId);
+      console.log(userId);
 
-		if (filePath) {
-			client.to(room).emit('new-message', { message: { message: message, filePath }, author: { username: user?.username }, createdAt: new Date() });
-		} else {
-			console.log('message', message, room)
-			client.to(room).emit('new-message', { message: { message: message }, author: { username: user?.username }, createdAt: new Date() });
-		}
-	}
+      await Promise.all(rooms.map(async (room) => {
+         const roomId = new Types.ObjectId(room);
+         await roomService.leaveRoom(new Types.ObjectId(userId), roomId);
+         await client.leave(room);
+      }));
+      await Promise.all(rooms.map((room) => client.leave(room)))
+      client.to(rooms).emit('user-left', { message: `${user!.username} leave room` })
+   };
+
+   public async sendMessageToRoom(client: Socket, userId: string, { room, message, filePath }: { room: string, message: string, filePath?: string }) {
+      const user = await UserModel.findById(userId);
+      await messageService.saveMessage(new Types.ObjectId(userId), new Types.ObjectId(room), message, filePath);
+
+      if (filePath) {
+         client.to(room).emit('new-message', { message: { message: message, filePath }, author: { username: user?.username }, createdAt: new Date() });
+      } else {
+         client.to(room).emit('new-message', { message: { message: message }, author: { username: user?.username }, createdAt: new Date() });
+      }
+   }
 };
 
 export const roomWsController = new RoomWsController();
--- src/services/room.services.ts
+++ src/services/room.services.ts
@@ -4,154 +4,162 @@ import { HttpError } from "../common/errors/http.error";
 import { ChatRoom, ChatRoomModel } from "../models/room.model";
 import { UserModel } from "../models/user.model";
 import { MessageModel } from "../models/message.model";
+import { UpdateRoomParams } from "../common/types-and-interfaces";
 
 export class RoomService {
 
-	async createRoom(
-		roomName: string,
-		description: string,
-		userId: Types.ObjectId
-	): Promise<ChatRoom> {
-		const user = await UserModel.findById(userId);
-		if (!user) {
-			throw new HttpError(StatusCodes.NOT_FOUND, "User is not found", "UserController");
-		}
+   async createRoom(
+      roomName: string,
+      description: string,
+      userId: Types.ObjectId
+   ): Promise<ChatRoom> {
+      const user = await UserModel.findById(userId);
+      if (!user) {
+         throw new HttpError(StatusCodes.NOT_FOUND, "User is not found", "RoomController");
+      }
 
-		const existingRoom = await ChatRoomModel.findOne({ name: roomName });
-		if (existingRoom) {
-			throw new HttpError(StatusCodes.CONFLICT, `A room with the name "${roomName}" already exists`, "UserController");
-		}
+      const existingRoom = await ChatRoomModel.findOne({ name: roomName });
+      if (existingRoom) {
+         throw new HttpError(StatusCodes.CONFLICT, `A room with the name "${roomName}" already exists`, "RoomController");
+      }
 
-		const newRoom = await ChatRoomModel.create({
-			name: roomName,
-			description,
-			owner: user
-		});
-		user.rooms.push(newRoom._id);
-		await user.save();
-		await newRoom.save();
+      const newRoom = await ChatRoomModel.create({
+         name: roomName,
+         description,
+         owner: user
+      });
+      user.rooms.push(newRoom._id);
+      await user.save();
+      await newRoom.save();
 
-		return newRoom;
-	}
+      return newRoom;
+   }
 
-	async deleteRoom(
-		roomId: Types.ObjectId,
-		userId: Types.ObjectId
-	) {
-		const user = await UserModel.findById(userId);
-		if (!user) {
-			throw new HttpError(StatusCodes.NOT_FOUND, "User is not found", "UserController");
-		}
-		const room = await ChatRoomModel.findById(roomId);
-		if (!room) {
-			throw new HttpError(StatusCodes.NOT_FOUND, "Room is not found", "UserController");
-		}
+   async deleteRoom(
+      roomId: Types.ObjectId,
+      userId: Types.ObjectId
+   ) {
+      const user = await UserModel.findById(userId);
+      if (!user) {
+         throw new HttpError(StatusCodes.NOT_FOUND, "User is not found", "RoomController");
+      }
+      const room = await ChatRoomModel.findById(roomId);
+      if (!room) {
+         throw new HttpError(StatusCodes.NOT_FOUND, "Room is not found", "RoomController");
+      }
 
-		if (!user.rooms.includes(roomId)) {
-			throw new HttpError(StatusCodes.FORBIDDEN, "User does not own this room", "UserController");
-		}
+      if (!user.rooms.includes(roomId)) {
+         throw new HttpError(StatusCodes.FORBIDDEN, "User does not own this room", "RoomController");
+      }
 
-		await ChatRoomModel.findByIdAndDelete(roomId);
-		await MessageModel.deleteMany({ room: roomId })
-		user.rooms = user.rooms.filter((roomId) => roomId.toString() !== room._id.toString());
-		await user.save();
-	}
+      await ChatRoomModel.findByIdAndDelete(roomId);
+      await MessageModel.deleteMany({ room: roomId })
+      user.rooms = user.rooms.filter((roomId) => roomId.toString() !== room._id.toString());
+      await user.save();
+   }
 
-	async updateRoom(
-		roomId: Types.ObjectId,
-		userId: Types.ObjectId,
-		params: object
-	) {
-		const user = await UserModel.findById(userId);
-		if (!user) {
-			throw new HttpError(StatusCodes.NOT_FOUND, "User is not found", "UserController");
-		}
-		const room = await ChatRoomModel.findById(roomId);
-		if (!room) {
-			throw new HttpError(StatusCodes.NOT_FOUND, "Room is not found", "UserController");
-		}
+   async updateRoom(
+      roomId: Types.ObjectId,
+      userId: Types.ObjectId,
+      params: UpdateRoomParams
+   ) {
+      const user = await UserModel.findById(userId);
+      if (!user) {
+         throw new HttpError(StatusCodes.NOT_FOUND, "User is not found", "RoomController");
+      }
+      const room = await ChatRoomModel.findById(roomId);
+      if (!room) {
+         throw new HttpError(StatusCodes.NOT_FOUND, "Room is not found", "RoomController");
+      }
 
-		if (!user.rooms.includes(roomId)) {
-			throw new HttpError(StatusCodes.FORBIDDEN, "User does not own this room", "UserController");
-		}
+      if (!user.rooms.includes(roomId)) {
+         throw new HttpError(StatusCodes.FORBIDDEN, "User does not own this room", "RoomController");
+      }
 
-		await ChatRoomModel.findByIdAndUpdate(
-			roomId,
-			params,
-			{ new: true }
-		);
-		const updatedRoom = await room.save();
-		return updatedRoom;
-	}
+      if (params.name !== room.name) {
+         const existingRoom = await ChatRoomModel.findOne({ name: params.name });
+         if (existingRoom) {
+            throw new HttpError(StatusCodes.CONFLICT, `A room with the name "${params.name}" already exists`, "RoomController");
+         }
+      }
 
-	async getRoom(roomId: Types.ObjectId): Promise<ChatRoom> {
-		const room = await ChatRoomModel.findById(roomId);
-		if (!room) {
-			throw new HttpError(StatusCodes.NOT_FOUND, "Room not found");
-		}
-		return room;
-	}
+      await ChatRoomModel.findByIdAndUpdate(
+         roomId,
+         params,
+         { new: true }
+      );
+      const updatedRoom = await room.save();
+      return updatedRoom;
+   }
 
-	async getOwnRooms(userId: Types.ObjectId): Promise<ChatRoom[]> {
-		const user = await UserModel.findById(userId);
-		if (!user) {
-			throw new HttpError(StatusCodes.NOT_FOUND, "User is not found", "UserController");
-		}
-		return await ChatRoomModel.find({ owner: user });
-	}
+   async getRoom(roomId: Types.ObjectId): Promise<ChatRoom> {
+      const room = await ChatRoomModel.findById(roomId);
+      if (!room) {
+         throw new HttpError(StatusCodes.NOT_FOUND, "Room not found");
+      }
+      return room;
+   }
 
-	async getOtherRooms(userId: Types.ObjectId): Promise<ChatRoom[]> {
-		const user = await UserModel.findById(userId);
-		if (!user) {
-			throw new HttpError(StatusCodes.NOT_FOUND, "User is not found", "UserController");
-		}
-		return await ChatRoomModel.find({ owner: { $ne: user } });
-	}
+   async getOwnRooms(userId: Types.ObjectId): Promise<ChatRoom[]> {
+      const user = await UserModel.findById(userId);
+      if (!user) {
+         throw new HttpError(StatusCodes.NOT_FOUND, "User is not found", "RoomController");
+      }
+      return await ChatRoomModel.find({ owner: user });
+   }
 
-	async joinRoom(userId: Types.ObjectId, roomId: Types.ObjectId) {
-		const room = await ChatRoomModel.findById(roomId);
-		if (!room) {
-			throw new HttpError(StatusCodes.NOT_FOUND, "Room is not found", "UserController");
-		}
+   async getOtherRooms(userId: Types.ObjectId): Promise<ChatRoom[]> {
+      const user = await UserModel.findById(userId);
+      if (!user) {
+         throw new HttpError(StatusCodes.NOT_FOUND, "User is not found", "RoomController");
+      }
+      return await ChatRoomModel.find({ owner: { $ne: user } });
+   }
 
-		const user = await UserModel.findById(userId);
-		if (!user) {
-			throw new HttpError(StatusCodes.NOT_FOUND, "User is not found", "UserController");
-		}
+   async joinRoom(userId: Types.ObjectId, roomId: Types.ObjectId) {
+      const room = await ChatRoomModel.findById(roomId);
+      if (!room) {
+         throw new HttpError(StatusCodes.NOT_FOUND, "Room is not found", "RoomController");
+      }
 
-		const userRooms = await ChatRoomModel.find({ participants: user._id });
-		for (const userRoom of userRooms) {
-			if (userRoom._id.toString() !== roomId.toString()) {
-				userRoom.participants = userRoom.participants.filter((id) => id.toString() !== userId.toString());
-				await userRoom.save();
-			}
-		}
+      const user = await UserModel.findById(userId);
+      if (!user) {
+         throw new HttpError(StatusCodes.NOT_FOUND, "User is not found", "RoomController");
+      }
 
-		room.participants.push(user._id);
-		await room.save();
-		return room;
-	}
+      const userRooms = await ChatRoomModel.find({ participants: user._id });
+      for (const userRoom of userRooms) {
+         if (userRoom._id.toString() !== roomId.toString()) {
+            userRoom.participants = userRoom.participants.filter((id) => id.toString() !== userId.toString());
+            await userRoom.save();
+         }
+      }
 
-	async leaveRoom(userId: Types.ObjectId, roomId: Types.ObjectId) {
-		const room = await ChatRoomModel.findById(roomId);
-		if (!room) {
-			throw new HttpError(StatusCodes.NOT_FOUND, "Room is not found", "UserController");
-		}
+      room.participants.push(user._id);
+      await room.save();
+      return room;
+   }
 
-		const userIndex = room.participants.indexOf(userId);
-		if (userIndex === -1) {
-			throw new HttpError(StatusCodes.CONFLICT, "User is not a participant in this room", "UserController");
-		}
+   async leaveRoom(userId: Types.ObjectId, roomId: Types.ObjectId) {
+      const room = await ChatRoomModel.findById(roomId);
+      if (!room) {
+         throw new HttpError(StatusCodes.NOT_FOUND, "Room is not found", "RoomController");
+      }
 
-		room.participants.splice(userIndex, 1);
-		await room.save();
-		return room;
-	}
+      const userIndex = room.participants.indexOf(userId);
+      if (userIndex === -1) {
+         throw new HttpError(StatusCodes.CONFLICT, "User is not a participant in this room", "RoomController");
+      }
 
-	async getRoomMessages(roomId: Types.ObjectId) {
-		const messages = await MessageModel.find({ room: roomId });
-		return messages;
-	}
+      room.participants.splice(userIndex, 1);
+      await room.save();
+      return room;
+   }
+
+   async getRoomMessages(roomId: Types.ObjectId) {
+      const messages = await MessageModel.find({ room: roomId });
+      return messages;
+   }
 }
 
 export const roomService = new RoomService();
