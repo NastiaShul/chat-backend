76e05d4076a46ee85496dff076daab48df106e03
Nastya <shulhaevich@gmail.com>
2023-05-03 11:54:09 +0300

    moved file folder creation to src

--- src/common/utils/fileHelpers.ts
+++ src/common/utils/fileHelpers.ts
@@ -5,7 +5,7 @@ import { join } from 'path';
 export const upload = multer({
    storage: multer.diskStorage({
       destination: async (req, _, cb) => {
-         const dirPath = join(__dirname, '../../../public/files')
+         const dirPath = join(__dirname, '../../public/files')
          // if the folder isn't in the project -> will create the folder
          if (!fs.existsSync(dirPath)) {
             fs.mkdirSync(dirPath, { recursive: true })
