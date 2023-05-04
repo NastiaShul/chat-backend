9f4e710790157859891aab755c388ec9dd2bfca4
Nastya <shulhaevich@gmail.com>
2023-05-04 11:51:17 +0300

    moved file folder creation

--- src/common/handlers/download.handler.ts
+++ src/common/handlers/download.handler.ts
@@ -5,7 +5,7 @@ const downloadHandler: RequestHandler = (req: Request, res: Response) => {
    if (!fileName) {
       return res.status(400);
    }
-   const filePath = `${__dirname}../../../public/${fileName}`;
+   const filePath = `${__dirname}../../../../public/${fileName}`;
    res.download(filePath); // Set disposition and send it.
 };
 
